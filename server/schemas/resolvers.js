389a70cde921fe3abc83331e5f047f18ce7f7826
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-08-05 15:34:50 -0700

    remove Contact from imported models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User, Product, Vendor, Contact } = require('../models/');
+const { User, Product, Vendor } = require('../models/');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
