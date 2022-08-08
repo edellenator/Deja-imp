e3f1896df2ef31daf395e983bfd5cdd40e4a19bd
Debbie Neflas <debbieneflas@gmail.com>
2022-08-07 22:18:50 -0700

    use queries on users page, update typedef for me

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,39 +1,14 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_USERS } from "../utils/queries";
 
 const UserList = () => {
-  // temp test data
-  const users = [
-    {
-      _id: 1,
-      username: "user1",
-      email: "user1@email.com",
-    },
-    {
-      _id: 2,
-      username: "user2",
-      email: "user2@email.com",
-    },
-    {
-      _id: 3,
-      username: "user3",
-      email: "user3@email.com",
-    },
-    {
-      _id: 4,
-      username: "user4",
-      email: "user4@email.com",
-    },
-    {
-      _id: 5,
-      username: "user5",
-      email: "user5@email.com",
-    },
-    {
-      _id: 6,
-      username: "user6",
-      email: "user6@email.com",
-    },
-  ];
+  const { loading, data } = useQuery(QUERY_USERS);
+  const users = data?.users || [];
+
+  if (loading) {
+    return <h4 className="text-center">LOADING...</h4>;
+  }
 
   return (
     <table className="table m-5">
@@ -41,6 +16,9 @@ const UserList = () => {
         <tr className="text-center">
           <th className="m-5" scope="col">
             <h3>Other Users</h3>
+          </th>
+          <th className="m-5" scope="col">
+            <h3>Emails</h3>
           </th>
         </tr>
       </thead>
