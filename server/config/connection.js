eae880367c1a3b213b37843da339704f32178699
Erik Dell <edellenator@gmail.com>
2022-08-08 18:58:34 -0700

    seeded database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/deja-imp", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/deja-imp", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
