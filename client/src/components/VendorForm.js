8a6b93fdb6fb0cb6d47cdf51f19c3f4b1d434089
Debbie Neflas <debbieneflas@gmail.com>
2022-08-08 23:30:33 -0700

    add vendor functionality

--- client/src/components/VendorForm.js
+++ client/src/components/VendorForm.js
@@ -1,138 +1,242 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_VENDOR, UPDATE_VENDOR } from "../utils/mutations";
+import { capitalizeFirstLetter } from "../utils/helpers";
 
-const VendorForm = ({ vendorFormData, setVendorFormData, id }) => {
-  const handleChange = (event) => {
+const VendorForm = (props) => {
+  const {
+    vendorFormData,
+    setVendorFormData,
+    contactFormData,
+    setContactFormData,
+    emptyContactForm,
+    emptyVendorForm,
+    id,
+  } = props;
+
+  const [showModal, setShowModal] = useState(false);
+  const [addVendor, { loading: addLoading, error: addError }] =
+    useMutation(ADD_VENDOR);
+  //   const [updateVendor, { loading: updateLoading, error: updateError }] =
+  //     useMutation(UPDATE_VENDOR);
+  //   const [updateContact, { loading: contactLoading, error: contactError }] = useMutation(UPDATE_CONTACT);
+
+  const handleReset = () => {
+    setContactFormData(emptyContactForm);
+    setVendorFormData(emptyVendorForm);
+  };
+  const handleChangeVendor = (event) => {
     const { name, value } = event.target;
-    setVendorFormData({
-      ...vendorFormData,
-      [name]: value,
-    });
+    if (name === "zip") {
+      setVendorFormData({
+        ...vendorFormData,
+        zip: parseInt(value),
+      });
+    } else {
+      setVendorFormData({
+        ...vendorFormData,
+        [name]: value,
+      });
+    }
     console.log(vendorFormData);
   };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+
+  const handleChangeContact = (event) => {
+    const { name, value } = event.target;
+    setContactFormData({
+      ...contactFormData,
+      [name]: value,
+    });
+    console.log(contactFormData);
   };
 
-  const {
-    vendorName,
-    vendorContact,
-    title,
-    email,
-    phone,
-    street,
-    city,
-    state,
-    zip,
-    notes,
-  } = vendorFormData;
+  const handleAddVendor = async (event) => {
+    event.preventDefault();
+    console.log(vendorFormData, contactFormData);
+    const { contactName, title, email, phoneNumber } = contactFormData;
+    try {
+      await addVendor({
+        variables: {
+          input: vendorFormData,
+          contactName,
+          title,
+          email,
+          phoneNumber,
+        },
+      });
+      setShowModal(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdateVendor = async (event) => {
+    event.preventDefault();
+    try {
+      await updateVendor({
+        variables: { input: vendorFormData },
+      });
+      await updateContact({ variables: { input: contactFormData } });
+      setShowModal(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setVendorFormData(emptyVendorForm);
+    setContactFormData(emptyContactForm);
+  };
+
+  const { vendorName, street, city, state, zip, notes } = vendorFormData;
+
+  const { contactName, title, email, phoneNumber } = contactFormData;
 
   return (
-    <form className="container" onSubmit={handleSubmit}>
-      <h3>Add a Vendor</h3>
-      <div className="flex-row">
-        <div className="col-7 mr-5">
-          <label className="form-label" htmlFor="vendorName">
-            Vendor Name
-          </label>
-          <input
-            className="form-input"
-            name="vendorName"
-            type="text"
-            value={vendorName}
-            onChange={handleChange}
-          />
-          <label className="form-label" htmlFor="vendorContact">
-            Vendor Contact
-          </label>
-          <input
-            className="form-input"
-            name="vendorContact"
-            type="text"
-            value={vendorContact}
-            onChange={handleChange}
-          />
-          <label className="form-label" htmlFor="title">
-            Title
-          </label>
-          <input className="form-input" name="title" type="text" />
-          <label className="form-label" htmlFor="email" value={title}>
-            Email
-          </label>
-          <input
-            className="form-input"
-            name="email"
-            type="email"
-            value={email}
-            onChange={handleChange}
-          />
-          <label className="form-label" htmlFor="phone">
-            Phone
-          </label>
-          <input
-            className="form-input"
-            name="phone"
-            type="tel"
-            value={phone}
-            onChange={handleChange}
-          />
-          <label className="form-label" htmlFor="street">
-            Street
-          </label>
-          <input
-            className="form-input"
-            name="street"
-            type="text"
-            value={street}
-            onChange={handleChange}
-          />
-          <label className="form-label" htmlFor="city">
-            City
-          </label>
-          <input
-            className="form-input"
-            name="city"
-            type="text"
-            value={city}
-            onChange={handleChange}
-          />
-          <label className="form-label" htmlFor="state">
-            State
-          </label>
-          <input
-            className="form-input"
-            name="state"
-            type="text"
-            value={state}
-            onChange={handleChange}
-          />
-          <label className="form-label" htmlFor="zip">
-            Zip Code
-          </label>
-          <input
-            className="form-input"
-            name="zip"
-            type="text"
-            value={zip}
-            onChange={handleChange}
-          />
+    <div className="container">
+      {showModal ? (
+        <div className="flex-row justify-center my-custom">
+          <div className="card w-50 py-5 text-center ">
+            <p>
+              {capitalizeFirstLetter(vendorName)} has been{" "}
+              {id ? "updated" : "added"}!
+            </p>
+            <button className="btn w-25" onClick={handleCloseModal}>
+              close
+            </button>
+          </div>
         </div>
-        <div className="col-4">
-          <label className="form-label" htmlFor="notes">
-            Notes
-          </label>
-          <textarea
-            className="form-textarea"
-            rows="12"
-            value={notes}
-            onChange={handleChange}
-          />
-          <button type="submit" className="btn">
-            {id ? "Edit Vendor" : "Add Vendor"}
-          </button>
-        </div>
-      </div>
-    </form>
+      ) : (
+        <form
+          onSubmit={id ? handleUpdateVendor : handleAddVendor}
+          onReset={handleReset}
+        >
+          <h3>Add a Vendor</h3>
+          <div className="flex-row">
+            <div className="col-7 mr-5">
+              <label className="form-label" htmlFor="vendorName">
+                Vendor Name
+              </label>
+              <input
+                className="form-input"
+                name="vendorName"
+                type="text"
+                value={vendorName}
+                onChange={handleChangeVendor}
+              />
+              <label className="form-label" htmlFor="contactName">
+                Vendor Contact
+              </label>
+              <input
+                className="form-input"
+                name="contactName"
+                type="text"
+                value={contactName}
+                onChange={handleChangeContact}
+              />
+              <label className="form-label" htmlFor="title">
+                Title
+              </label>
+              <input
+                className="form-input"
+                name="title"
+                type="text"
+                value={title}
+                onChange={handleChangeContact}
+              />
+              <label className="form-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="form-input"
+                name="email"
+                type="email"
+                value={email}
+                onChange={handleChangeContact}
+              />
+              <label className="form-label" htmlFor="phoneNumber">
+                Phone
+              </label>
+              <input
+                className="form-input"
+                name="phoneNumber"
+                type="tel"
+                value={phoneNumber}
+                onChange={handleChangeContact}
+              />
+              <label className="form-label" htmlFor="street">
+                Street
+              </label>
+              <input
+                className="form-input"
+                name="street"
+                type="text"
+                value={street}
+                onChange={handleChangeVendor}
+              />
+              <label className="form-label" htmlFor="city">
+                City
+              </label>
+              <input
+                className="form-input"
+                name="city"
+                type="text"
+                value={city}
+                onChange={handleChangeVendor}
+              />
+              <label className="form-label" htmlFor="state">
+                State
+              </label>
+              <input
+                className="form-input"
+                name="state"
+                type="text"
+                value={state}
+                onChange={handleChangeVendor}
+              />
+              <label className="form-label" htmlFor="zip">
+                Zip Code
+              </label>
+              <input
+                className="form-input"
+                name="zip"
+                type="text"
+                value={zip}
+                onChange={handleChangeVendor}
+              />
+            </div>
+            <div className="col-4">
+              {/* <label className="form-label" htmlFor="notes">
+                Notes
+              </label>
+              <textarea
+                className="form-textarea"
+                rows="12"
+                value={notes}
+                onChange={handleChangeVendor}
+              /> */}
+              <button type="reset" className="btn">
+                {id ? "Reset Form" : "Clear Form"}
+              </button>
+              {/* {addLoading || updateLoading ? ( */}
+              {addLoading ? (
+                <button className="btn">Submitting...</button>
+              ) : (
+                <button type="submit" className="btn">
+                  {id ? "Edit Vendor" : "Add Vendor"}
+                </button>
+              )}
+              {/* {addError ||
+                (updateError && ( */}
+              {addError && (
+                <p className="text-tertiary">Something went wrong!</p>
+              )}
+            </div>
+          </div>
+        </form>
+      )}
+    </div>
   );
 };
 
--- client/src/pages/EditVendor.js
+++ client/src/pages/EditVendor.js
@@ -1,143 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import VendorForm from "../components/VendorForm";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_VENDOR } from "../utils/queries";
 
 const EditVendor = () => {
-    // temp vendor data
-    const vendors = [
-        {
-            id: "1",
-            name: "Vendor 1",
-            contacts: [
-                {
-                    id: "1",
-                    name:"Erik Dell",
-                    title: "Da Baby",
-                    email: "edellenator@gmail.com",
-                    phone: "555-555-5555",
-                },
-                {
-                    id: "2",
-                    name:"Joel Abankwah",
-                    title: "Da Ba-ba-ba-b0ss",
-                    email: "daBoss@gmail.com",
-                    phone: "555-555-5555",
-                }
-            ],
-            street: "1234 vendor street",
-            city: "Vendorville",
-            state: "Vendorginia",
-            zip: "12345",
-            notes: [
-                {
-                    id: '1',
-                    text:"This vendor has a note",
-                    dateCreated:"10/10/2020 6:00pm",
-                    author: "User 1"
-                },
-                {
-                    id: '2',
-                    text:"This vendor has another note",
-                    dateCreated:"10/10/2020 6:00pm",
-                    author: "User 2"
-                }
-            ],
-            products: [
-                {
-                    id: '1',
-                    name: 'item 1',
-                    stock: 100
-                },
-                {
-                    id: '2',
-                    name: 'item 2',
-                    stock: 50
-                }
-            ]
-        },
-        {
-            id: "2",
-            name: "Vendor 2",
-            contacts: [
-                {
-                    id: "1",
-                    name:"Debbie Neflas",
-                    title: "The Don",
-                    email: "TheDon@gmail.com",
-                    phone: "555-555-5555",
-                },
-                {
-                    id: "2",
-                    name:"Akeva Melchor",
-                    title: "El Jefe",
-                    email: "ElJefe@gmail.com",
-                    phone: "555-555-5555",
-                }
-            ],
-            street: "1234 vendor street",
-            city: "Vendorville",
-            state: "Vendorginia",
-            zip: "12345",
-            notes: [
-                {
-                    id: '1',
-                    text:"This vendor has a note",
-                    dateCreated:"10/10/2020 6:00pm",
-                    author: "User 1"
-                },
-                {
-                    id: '2',
-                    text:"This vendor has another note",
-                    dateCreated:"10/10/2020 6:00pm",
-                    author: "User 2"
-                }
-            ],
-            products: [
-                {
-                    id: '1',
-                    name: 'item 1',
-                    stock: 100
-                },
-                {
-                    id: '2',
-                    name: 'item 2',
-                    stock: 50
-                }
-            ]
-        },
-    ];
+  const emptyVendorForm = {
+    vendorName: "",
+    street: "",
+    city: "",
+    state: "",
+    zip: "",
+    notes: "",
+  };
 
-    
+  const emptyContactForm = {
+    contactName: "",
+    title: "",
+    email: "",
+    phoneNumber: "",
+  };
 
-    const {id: idParam} = useParams();
-    console.log(idParam)
+  const [contactFormData, setContactFormData] = useState(emptyContactForm);
+  const [vendorFormData, setVendorFormData] = useState(emptyVendorForm);
 
-    const vendor = vendors.find((vendor) => {
-        return vendor.id === idParam
+  const { id: idParam } = useParams();
+  console.log(idParam);
+
+  const { loading, data } = useQuery(QUERY_VENDOR, {
+    variables: { id: idParam },
+  });
+  const vendor = data?.vendor || [];
+
+  const { vendorName, street, city, state, zip, notes } = vendor;
+
+  if (data) {
+    setVendorFormData({
+      vendorName,
+      street,
+      city,
+      state,
+      zip,
+      notes,
     });
+    // setContactFormData({
+    //   contactName,
+    //   title,
+    //   phoneNumber,
+    //   email,
+    // });
+  }
 
-    const [vendorFormData, setVendorFormData] = useState({
-        vendorName: vendor.name,
-        vendorContact: "",
-        title: "",
-        email: "",
-        phone: "",
-        street: vendor.street,
-        city: vendor.city,
-        state: vendor.state,
-        zip: vendor.zip,
-        notes: "",
-    });
-
-
-
-    return (
-        <VendorForm 
-            vendorFormData={vendorFormData} 
-            setVendorFormData={setVendorFormData}
-            id={idParam} 
-        />
-    )
-}
-
+  return (
+    <VendorForm
+      vendorFormData={vendorFormData}
+      setVendorFormData={setVendorFormData}
+      //   emptyVendorForm={emptyVendorForm}
+      //   contactFormData={contactFormData}
+      setContactFormData={setContactFormData}
+      //   emptyContactForm={emptyContactForm}
+      id={idParam}
+    />
+  );
+};
 export default EditVendor;
